fc0d6f3827f1027fb1adb2d31d0f3a60d9460456
suada <suadaljuaid1@gmail.com>
2025-11-16 21:54:55 +0300

    Lab3- Arrays - part1

--- src/Arrays/Arrayss.java
+++ src/Arrays/Arrayss.java
@@ -1,4 +1,22 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Arrayss {
+    Scanner scanner=new Scanner(System.in);
+
+ public void input(int []a){
+     for(int i=0;i<a.length;i++)
+         a[i]=scanner.nextInt();
+
+ }
+
+ public  void traversal(int []a){
+
+     for (int i=0;i<a.length;i++){
+         System.out.println("Index["+i+"]"+"="+a[i]);
+     }
+ }
+
+
 }
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,7 +1,47 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Arrayss arr = new Arrayss();
+       // int []numbers={1,2,3,4,5};
+
+        int []numbers2=new int[6];
+        System.out.println("PLease enter  "+numbers2.length+"  elements:");
+        arr.input(numbers2);
+        arr.traversal(numbers2);
+
+
+
+
+
+
+
+
+
+//
+//        for (int num:numbers2)
+//            System.out.println("n= "+num);
+//
+
+
+
+
+        //1
+       // System.out.println(Arrays.toString(numbers2));
+
+        //2
+
+
+        //3
+//        for (int i=0;i<numbers.length;i++){
+//            System.out.println("Index["+i+"]"+"="+numbers[i]);
+//        }
+
+
 
     }
 }
