0a3452e3893962fa45b9503b5efbadedc4ae0839
suada <suadaljuaid1@gmail.com>
2025-11-23 21:29:58 +0300

    Lab3.1- Arrays - part2

--- src/Arrays/Arrayss.java
+++ src/Arrays/Arrayss.java
@@ -16,7 +16,37 @@ public class Arrayss {
      for (int i=0;i<a.length;i++){
          System.out.println("Index["+i+"]"+"="+a[i]);
      }
+
+
  }
 
+
+ public  void Reverse_traversal(int []a) {
+
+     for (int i = a.length - 1; i >= 0; i--) {
+         System.out.println("Index[" + i + "]" + "=" + a[i]);
+     }
+ }
+
+ public void Traversal2(int a[]){
+     for(int e:a)
+         System.out.println(e);
+
+ }
+
+ public void update(int[]a,int index, int nvalue){
+     if(index<0 || index>=a.length)
+         System.out.println("Index not found");
+     else
+     a[index]=nvalue;
+
+ }
+    public void delete(int[]a,int index){
+        if(index<0 || index>=a.length)
+            System.out.println("Index not found");
+        else
+            a[index]=0;
+
+    }
 
 }
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -5,43 +5,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+
         Scanner scanner = new Scanner(System.in);
         Arrayss arr = new Arrayss();
-       // int []numbers={1,2,3,4,5};
+        int []numbers={1,2,3,4,5};
+        //numbers[2]=7;
 
-        int []numbers2=new int[6];
-        System.out.println("PLease enter  "+numbers2.length+"  elements:");
-        arr.input(numbers2);
-        arr.traversal(numbers2);
+       // arr.update(numbers,8,7);
+        arr.delete(numbers,2);
+        // arr.Reverse_traversal(numbers2);
 
+        arr.traversal(numbers);
 
-
-
-
-
-
-
-
-//
-//        for (int num:numbers2)
-//            System.out.println("n= "+num);
-//
+        /// System.out.println("PLease enter  "+numbers2.length+"  elements:");
+        // arr.input(numbers2);
 
 
 
 
-        //1
-       // System.out.println(Arrays.toString(numbers2));
 
-        //2
+            //1
+            // System.out.println(Arrays.toString(numbers2));
+
+            //2
 
 
-        //3
+            //3
 //        for (int i=0;i<numbers.length;i++){
 //            System.out.println("Index["+i+"]"+"="+numbers[i]);
 //        }
 
 
-
+        }
     }
-}
